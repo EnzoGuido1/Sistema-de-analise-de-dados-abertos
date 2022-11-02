24afe2cc343fddc10254499c2dc989cee49d3470
Enzo Guido Americano da Costa <enzoguido.a375@uel.br>
2022-11-02 16:10:17 -0300

    xhtml and controller test for dataTable partido

--- dados-parlamentares/src/main/java/br/uel/bd1/dadosparlamentares/control/GenericController.java
+++ dados-parlamentares/src/main/java/br/uel/bd1/dadosparlamentares/control/GenericController.java
@@ -1,14 +1,15 @@
 package br.uel.bd1.dadosparlamentares.control;
 
+import br.uel.bd1.dadosparlamentares.model.Partido;
 import jakarta.inject.Inject;
 import java.io.Serializable;
 import java.util.List;
 
 public abstract class GenericController<T> implements Serializable {
     @Inject
-    private T bean;
-    private List<T> queryBuf;
-    private static final long serialVersionUID = 1L;
+    protected T bean;
+    protected List<T> queryBuf;
+    protected static final long serialVersionUID = 1L;
 
     public T getBean() {
         return bean;
